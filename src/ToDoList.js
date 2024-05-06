a3054bfc4714669e89e084553671b9f1ee5f0415
Ana Carolina <karols.15@hotmail.com>
2024-05-06 15:57:45 -0300

    adjust condition

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -42,7 +42,7 @@ function ToDoList() {
   };
 
   const moveTaskDown = (index) => {
-    if (index > tasks.length - 1) {
+    if (index < tasks.length - 1) {
       const updatedTasks = [...tasks];
       [updatedTasks[index], updatedTasks[index + 1]] = [
         updatedTasks[index + 1],
